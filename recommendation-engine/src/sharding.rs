630b0233a51cafc41777fff4eeb7e3fab3f2ac9a
Stephen Leyva <sleyva1297@gmail.com>
2020-09-06 10:50:21 -0700

    Implement struct for GeoShard

--- recommendation-engine/src/sharding.rs
+++ recommendation-engine/src/sharding.rs
@@ -4,48 +4,81 @@ use s2::cellid::CellID;
 const MIN_SHARD: i32 = 40;
 const MAX_SHARD: i32 = 100;
 
-pub fn standard_deviation(shards: &Vec<i32>) -> f64 {
-    let mean: f64 = shards.into_iter().sum::<i32>() as f64 / shards.len() as f64;
-
-    let varience: f64 = shards.into_iter()
-        .map(|x| (*x as f64 - mean) * (*x as f64 - mean)).sum::<f64>() / shards.len() as f64;
-    
-    varience.sqrt()
+pub struct Ranges {
+    start: CellID,
+    end: CellID
 }
 
-pub fn generate_shards(cell_load: &HashMap<CellID, i32>) -> Vec<i32> {
+pub struct Shard {
+    ranges: Ranges,
+    cell_count: i32,
+    cell_score: i32
+}
+
+pub fn generate_shards(cell_load: &HashMap<CellID, i32>) -> Vec<Shard> {
     let total: i32 = cell_load.into_iter()
         .fold(0, |sum, i| sum + i.1);
 
     let max_size = total / MIN_SHARD;
     let min_size = total / MAX_SHARD;
 
-    let mut best_shards: Vec<i32> = vec![];
+    let mut best_shards: Vec<Shard> = vec![];
     let mut min_standard_deviation = f64::MAX;
 
-    for i in min_size..max_size {
+    for container_size in min_size..max_size {
         let mut current_sum = 0;
-        let mut geo_shards = vec![];
-        for (_, cell_score) in cell_load {
-            if current_sum + cell_score < i {
+        let mut geo_shards = Vec::new();
+        for (cell_id, cell_score) in cell_load {
+            let ranges = Ranges {
+                start: *cell_id,
+                end: *cell_id,
+            };
+            let mut shard = Shard {
+                ranges,
+                cell_count: 0,
+                cell_score: 0,
+            };
+
+            if current_sum + cell_score < container_size {
                 current_sum += cell_score;
+                shard.cell_count += 1;
             } else {
-                geo_shards.push(current_sum);
-                current_sum += cell_score;
+                shard.ranges.end = *cell_id;
+                geo_shards.push(shard);
+                current_sum = *cell_score;
             }
         }
 
         if current_sum != 0 {
-            geo_shards.push(current_sum);
+            let last = cell_load.into_iter().last().unwrap();
+            let ranges = Ranges {
+                start: *last.0,
+                end: *last.0,
+            };
+            let shard = Shard {
+                ranges,
+                cell_count: 1,
+                cell_score: current_sum,
+            };
+            geo_shards.push(shard);
         }
 
-        let standard_dev = standard_deviation(&geo_shards);
+        let standard_dev = standard_deviation_between_shards(&geo_shards);
         if standard_dev < min_standard_deviation {
             min_standard_deviation = standard_dev;
             best_shards = geo_shards;
         }
     }
     best_shards
+}
+
+pub fn standard_deviation_between_shards(shards: &Vec<Shard>) -> f64 {
+    let mean: f64 = shards.into_iter().fold(0.0, |sum, x| sum + x.cell_score as f64) / shards.len() as f64;
+
+    let varience: f64 = shards.into_iter()
+        .map(|x| (x.cell_score as f64 - mean) * (x.cell_score as f64 - mean)).sum::<f64>() / shards.len() as f64;
+
+    varience.sqrt()
 }
 
 #[cfg(test)]
@@ -64,6 +97,20 @@ mod test {
                 lat: s1::Deg($lat).into(),
                 lng: s1::Deg($lng).into(),
             }
+        };
+    }
+
+    macro_rules! shard {
+        ($cell_score:expr) => {
+            Shard {
+                ranges: Ranges {
+                    start: CellID::from_face(8),
+                    end: CellID::from_face(8),
+                },
+                cell_count: 0,
+                cell_score: $cell_score,
+            };
+
         };
     }
 
@@ -95,8 +142,30 @@ mod test {
 
     #[test]
     fn test_standard_deviation() {
-        let nums = vec![ 9, 2, 5, 4, 12, 7, 8, 11, 9, 3, 7, 4, 12, 5, 4, 10, 9, 6, 9, 4 ];
-        let standard_dev = standard_deviation(&nums);
+        let shards = vec![
+            shard!(9),
+            shard!(2),
+            shard!(5),
+            shard!(4),
+            shard!(12),
+            shard!(7),
+            shard!(8),
+            shard!(11),
+            shard!(9),
+            shard!(3),
+            shard!(7),
+            shard!(4),
+            shard!(12),
+            shard!(5),
+            shard!(4),
+            shard!(10),
+            shard!(9),
+            shard!(6),
+            shard!(9),
+            shard!(4),
+        ];
+
+        let standard_dev = standard_deviation_between_shards(&shards);
         assert_eq!(standard_dev, 2.9832867780352594_f64)
     }
 }
